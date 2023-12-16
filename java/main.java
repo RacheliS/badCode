c8c814d59fc5c838ae5a4faf6883777be1dacea3
Racheli Amrusi <Racheli.Sulemani@exscientia.co.il>
2023-12-17 00:31:38 +0200

    Update main.java

--- java/main.java
+++ java/main.java
@@ -30,6 +30,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
+      
     http.csrf().ignoringAntMatchers("/route/fre");
   }
 }
